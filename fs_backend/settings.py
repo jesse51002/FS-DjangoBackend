67b1213813502a550b7c6aae08ab1f70c3ce2825
Jesse Musa <jessemusa2@gmail.com>
2024-01-03 15:17:22 -0600

    Added local host to allowed ports for testing with flutter

--- fs_backend/settings.py
+++ fs_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l!=orbc)^6%r!2d*9ojq--(doav^kh)x+2bo!c=n^d*!3qgz+7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.2.2']
 
 
 # Application definition
