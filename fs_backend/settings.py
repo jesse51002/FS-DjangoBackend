6b58317dfae287fd53d1a96e0f234e68b69f9709
Jesse Musa <jessemusa2@gmail.com>
2024-01-03 15:24:09 -0600

    more hosts

--- fs_backend/settings.py
+++ fs_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l!=orbc)^6%r!2d*9ojq--(doav^kh)x+2bo!c=n^d*!3qgz+7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2']
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', '10.0.2.2']
 
 
 # Application definition
